fe5c6cd656110a7b4ea9e5eafe8a6755d7fd1918
Koenig-Heinrich-der-4te <moritz_nickel@icloud.com>
2024-12-01 22:04:36 +0100

    first commit

---
+++ 01.py
@@ -0,0 +1,22 @@
+with open("01.txt") as file:
+    data = file.read()
+
+rows = [line.split("   ") for line in data.split("\n")]
+left = [int(row[0]) for row in rows]
+right = [int(row[1]) for row in rows]
+
+left.sort()
+right.sort()
+
+total_distance = sum(abs(a - b) for a, b in zip(left, right))
+print("Part1: ", total_distance)
+
+left = {n: left.count(n) for n in set(left)}
+right = {n: right.count(n) for n in set(right)}
+
+similarity_score = 0
+
+for n, count in left.items():
+    similarity_score += n * count * right.get(n, 0)
+
+print("Part2:", similarity_score)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2024
+
+this repository contains my solutions to 2023's [Advent of Code](https://adventofcode.com/2024) Problems
+
+-   **Day 1** ([Part1 & Part2](01.py)): [Historian Hysteria](https://adventofcode.com/2024/day/1)
